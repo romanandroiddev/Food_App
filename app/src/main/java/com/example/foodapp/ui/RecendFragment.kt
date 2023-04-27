0f2bc591021ffeb7ffc916a493cbcfa6f76c67a4
alewatdinjaksilikov <jaksilikovalewatdin2004@gmail.com>
2023-04-27 22:15:54 +0500

    Commit

---
+++ app/src/main/java/com/example/foodapp/ui/RecendFragment.kt
@@ -0,0 +1,14 @@
+package com.example.foodapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.foodapp.R
+
+class RecendFragment:Fragment(R.layout.fragment_recent) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+}
